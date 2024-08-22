5d870b89b6dd253776eba649c2b4de5ba9b42236
Mike Barker <mike@thebarkers.com>
2024-08-22 23:31:27 +0000

    Add feature exercism-cli

---
+++ src/exercism-cli/NOTES.md
@@ -0,0 +1,25 @@
+# Exercism CLI
+
+Provides the `exercism` command at the shell. Using this tool requires authentication.
+
+```shell
+exercism configure --token=[TOKEN]
+```
+
+Please visit Exercism's [working locally](https://exercism.org/docs/using/solving-exercises/working-locally) documentation for further usage information.
+
+## The Working Directory
+
+The Exercism CLI usually uses a working directory in the `$HOME` directory of the user.
+To use the workspace directory of the devcontainer as Exercism's working directory, append the following to the `exercism configure` command: `--workspace="$(pwd)"`.
+
+## OS Support
+
+This feature currently only supports Debian based containers (where APT is used as package manager).
+`bash` is required to run the install script.
+
+## Processor Architecture Support
+
+The Exercism CLI is provided as a native application for a variety of processor architectures.
+This feature tries to determine the architecture of the current system and download the appropriate binary.
+As there is currently no need to support architectures other than arm64 (e.g. Apple Silicon) and x86_64 these are the only supported architectures.
---
+++ src/exercism-cli/install.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+#
+# Install the Exercism CLI tool
+# https://github.com/exercism/cli
+#
+# Dependencies: curl, cut
+#
+# Environment:
+#   - VERSION: The version to install [default: latest].
+#   - DESTDIR: The destination of the executable [default: /usr/local/bin/].
+
+set -e
+
+GITHUB_BASE_URL="https://github.com/exercism/cli"
+LATEST_URL="${GITHUB_BASE_URL}/releases/latest/"
+DESTDIR="${DESTDIR:-/usr/local/bin}"
+# OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+OS="linux"
+ARCH=$(uname -m)
+if [ "$ARCH" = "aarch64" ]; then
+    ARCH="arm64"
+fi
+
+
+apt_get_update()
+{
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" > /dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+check_packages curl ca-certificates
+
+# Get the latest version of the exercism cli
+if [[ -z "$VERSION" ]] || [[ "$VERSION" = "latest" ]]; then {
+    VERSION=$(curl -sLI -o /dev/null -w '%{url_effective}' "$LATEST_URL" | cut -d "v" -f 2)
+} fi
+
+echo "Installing exercism-cli: v${VERSION}"
+
+RELEASE_URL="${GITHUB_BASE_URL}/releases/download/v${VERSION}/exercism-${VERSION}-${OS}-${ARCH}.tar.gz"
+echo $RELEASE_URL
+
+DOWNLOAD_DIR=$(mktemp -d || mktemp -d -t 'tmp')
+echo $DOWNLOAD_DIR
+
+pushd "$DOWNLOAD_DIR"
+curl -sL --retry 3 "$RELEASE_URL" | tar -xz
+
+echo "Installing to $DESTDIR"
+install exercism "$DESTDIR"
+popd
+
+echo "Remove $DOWNLOAD_DIR"
+rm -r "$DOWNLOAD_DIR"
